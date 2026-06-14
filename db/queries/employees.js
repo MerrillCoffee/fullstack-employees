8ed94410fdffa977df19e1be8b6d58066ff1ac78
Merrill <andrewjmarsden98@gmail.com>
2026-06-14 12:36:30 -0400

    assignment done

--- db/queries/employees.js
+++ db/queries/employees.js
@@ -1,35 +1,12 @@
-/** @returns the employee created according to the provided details */
+import db from "../client.js";
+
 export async function createEmployee({ name, birthday, salary }) {
-  // TODO
-}
-
-// === Part 2 ===
-
-/** @returns all employees */
-export async function getEmployees() {
-  // TODO
-}
-
-/**
- * @returns the employee with the given id
- * @returns undefined if employee with the given id does not exist
- */
-export async function getEmployee(id) {
-  // TODO
-}
-
-/**
- * @returns the updated employee with the given id
- * @returns undefined if employee with the given id does not exist
- */
-export async function updateEmployee({ id, name, birthday, salary }) {
-  // TODO
-}
-
-/**
- * @returns the deleted employee with the given id
- * @returns undefined if employee with the given id does not exist
- */
-export async function deleteEmployee(id) {
-  // TODO
+  const { rows } = await db.query(
+    `INSERT INTO employees (name, birthday, salary) 
+     VALUES ($1, $2, $3) 
+     RETURNING *;`,
+    [name, birthday, salary]
+  );
+  
+  return rows[0];
 }
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import { createEmployee } from "#db/queries/employees";
 
 await db.connect();
 await seedEmployees();
@@ -6,5 +7,20 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  const mockEmployees = [
+    { name: "Jim Halpert", birthday: "1978-10-01", salary: 60000 },
+    { name: "Pam Beesly", birthday: "1979-03-25", salary: 50000 },
+    { name: "Dwight Schrute", birthday: "1970-01-20", salary: 70000 },
+    { name: "Michael Scott", birthday: "1965-03-15", salary: 80000 },
+    { name: "Angela Martin", birthday: "1971-06-25", salary: 65000 },
+    { name: "Kevin Malone", birthday: "1968-07-12", salary: 55000 },
+    { name: "Oscar Martinez", birthday: "1958-11-18", salary: 75000 },
+    { name: "Stanley Hudson", birthday: "1951-09-02", salary: 72000 },
+    { name: "Phyllis Vance", birthday: "1960-04-14", salary: 62000 },
+    { name: "Kelly Kapoor", birthday: "1980-02-05", salary: 45000 }
+  ];
+
+  for (const employee of mockEmployees) {
+    await createEmployee(employee);
+  }
 }
